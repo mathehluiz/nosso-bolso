2a7f564f0394ead42824a701716da4f9936f181f
mathehluiz <clatclot09@gmail.com>
2024-08-24 14:16:08 -0300

    chore: removing unused events

--- src/shared/events/booking/booking-management.event.ts
+++
@@ -1,6 +0,0 @@
-export enum BookingManagementOperationType {
-  BOOKING_CREATED = 'booking.created',
-  BOOKING_UPDATED = 'booking.updated',
-  BOOKING_RESCHEDULED = 'booking.rescheduled',
-  BOOKING_CANCELLED = 'booking.cancelled',
-}
