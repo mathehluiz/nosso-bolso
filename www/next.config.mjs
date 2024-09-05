b8a74014729df41ed2ce361c1064327c8547cc26
mathehluiz <clatclot09@gmail.com>
2024-09-05 09:33:31 -0300

    deploy: moving to one docker-compose file

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
